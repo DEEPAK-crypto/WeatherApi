cf131bde41ee386cd99bae2277b6d7dad9d0cdc4
DEEPAK-crypto <deepsdeep45@gmail.com>
2020-05-16 21:27:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var request = require("request");
 
-request("https://api.openweathermap.org/data/2.5/weather?lat=30.4831&lon=76.595&appid=7beb6de85d3f3a28dabda1015684562f&units=metric&callback=test", function(error, response, body) {
-    if (error) {
-        console.log("Something went wrong!");
-        console.log(error);
-    } else if (response.statusCode == 200) {
-        console.log(body);
+request("https://api.openweathermap.org/data/2.5/weather?lat=30.4831&lon=76.595&appid=7beb6de85d3f3a28dabda1015684562f&units=metric", function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var parsedData = JSON.parse(body);
+        console.log(parsedData);
+
     }
+
 });
